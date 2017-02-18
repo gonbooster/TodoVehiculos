ae67f1799ddc3c90c8ad61815404eff47d257f6c
DESKTOP-CLRB3CH\Gonzalo <gonzalodelpalacio@gmail.com>
2017-02-18 12:24:29 +0100

    sorted root

--- core/conexion.php
+++ core/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$username = "u219197018_tv";
-$password = "tv1234";
-$hostname = "mysql.hostinger.co";
-$bd = "u219197018_tv"; 
+$username = "";
+$password = "";
+$hostname = "";
+$bd = ""; 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) or die("Imposible connectarse a MySQL");
 mysql_select_db($bd,$dbhandle)or die("No se puede localizar la base de datos indicada");
